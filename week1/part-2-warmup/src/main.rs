158f78157930595352f69f849bc3fd766de0f6b5
dentiny <1910921171@qq.com>
2021-07-05 22:23:52 -0700

    week1 part2

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -3,41 +3,48 @@
 use std::collections::HashSet;
 
 fn main() {
-    println!("Hi! Try running \"cargo test\" to run tests.");
+  println!("Hi! Try running \"cargo test\" to run tests.");
 }
 
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+  let mut vec: Vec<i32> = Vec::new();
+  for elem in v.iter() {
+    vec.push(elem + n);
+  }
+  vec
 }
 
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+  for elem in v.iter_mut() {
+    *elem += n;
+  }
 }
 
 fn dedup(v: &mut Vec<i32>) {
-    unimplemented!()
+  let mut values: HashSet<i32> = HashSet::new();
+  v.retain(|&x| values.insert(x));
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_add_n() {
-        assert_eq!(add_n(vec![1], 2), vec![3]);
-    }
+  #[test]
+  fn test_add_n() {
+    assert_eq!(add_n(vec![1], 2), vec![3]);
+  }
 
-    #[test]
-    fn test_add_n_inplace() {
-        let mut v = vec![1];
-        add_n_inplace(&mut v, 2);
-        assert_eq!(v, vec![3]);
-    }
+  #[test]
+  fn test_add_n_inplace() {
+    let mut v = vec![1];
+    add_n_inplace(&mut v, 2);
+    assert_eq!(v, vec![3]);
+  }
 
-    #[test]
-    fn test_dedup() {
-        let mut v = vec![3, 1, 0, 1, 4, 4];
-        dedup(&mut v);
-        assert_eq!(v, vec![3, 1, 0, 4]);
-    }
+  #[test]
+  fn test_dedup() {
+    let mut v = vec![3, 1, 0, 1, 4, 4];
+    dedup(&mut v);
+    assert_eq!(v, vec![3, 1, 0, 4]);
+  }
 }
